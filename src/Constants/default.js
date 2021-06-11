4e739b6f7c597562497046997d116cbb595d9f46
anil kumar <anirai.kumar@gmail.com>
2021-06-11 18:59:14 +0530

    added constants

---
+++ src/Constants/default.js
@@ -0,0 +1,9 @@
+// export const API_ENDPOINT_URL = 'http://localhost:8000'
+let API_ENDPOINT_URL;
+if (window.location.origin == "https://amplifyart.netlify.app") {
+    API_ENDPOINT_URL = "https://amplify-api.herokuapp.com";
+} else {
+    API_ENDPOINT_URL = 'http://localhost:8000';
+    // API_ENDPOINT_URL = 'https://amplify-api.herokuapp.com';
+}
+export { API_ENDPOINT_URL };
