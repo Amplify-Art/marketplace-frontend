6ec3ddabfacf0d9f72c17b337168b64dfed473e9
Jonathon <jonathon@pixel8.io>
2021-06-17 07:46:39 -0400

    minor ui fixes

--- src/Components/Parts/AddShowCase/index.js
+++ src/Components/Parts/AddShowCase/index.js
@@ -1,29 +1,30 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
 import './AddShowCase.scss';
 
 function AddShowCase({ showCaseData }) {
-    const [ loading, setLoading ] = useState(true);
-    
-    useEffect(() => setTimeout(() => setLoading(false), 5000),[]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <div id="addshowcase" >
-            <div class="scrollbar" id="style-4">
-                {showCaseData && showCaseData.length > 0 && showCaseData.map((showcase, item) => (
-                    <div className="row">
-                        {!loading ? <img src={showcase.image} alt="" /> : <Skeleton width={60} height={60}/>}
-                        <div className="row-wrap">
-                            <div className="row-title">{showcase.title}</div>
-                            <div className="row-desc">{showcase.description}</div>
-                        </div>
-                        <button className="add-btn">Add</button>
-                    </div>
-                ))}
+  useEffect(() => setTimeout(() => setLoading(false), 1000), []);
+
+  return (
+    <div id="addshowcase" >
+      <div class="scrollbar" id="style-4">
+        {showCaseData && showCaseData.length > 0 && showCaseData.map((showcase, item) => (
+          <div className="row">
+            <img src={showcase.image} className={`${loading && 'hidden'}`} />
+            {loading && <Skeleton width={60} height={60} />}
+            <div className="row-wrap">
+              <div className="row-title">{showcase.title}</div>
+              <div className="row-desc">{showcase.description}</div>
             </div>
-        </div>
-    )
+            <button className="add-btn">Add</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 };
 
 export default AddShowCase;
--- src/Components/Parts/CreatePlayList/index.js
+++ src/Components/Parts/CreatePlayList/index.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { useForm } from "react-hook-form";
 import AddShowCase from '../AddShowCase';
@@ -8,53 +8,53 @@ import CDImg from '../../../assets/images/cd-img.svg';
 import './CreatePlayList.scss';
 
 function CreatePlayList({ showCaseData }) {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [ loading, setLoading ] = useState(true);
-    
-    useEffect(() => setTimeout(() => setLoading(false), 5000),[]);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [loading, setLoading] = useState(true);
 
-    const playlistData = [
-        { title: "What is Love" },
-        { title: "What is Love" },
-        { title: "What is Love" },
-        { title: "What is Love" }
-    ];
+  useEffect(() => setTimeout(() => setLoading(false), 1000), []);
 
-    const renderPlayList = () => {
-        return playlistData.map((list, index) => (
-            <div>{`${index + 1}. ${list.title}`}</div>
-        ))
-    }
+  const playlistData = [
+    { title: "What is Love" },
+    { title: "What is Love" },
+    { title: "What is Love" },
+    { title: "What is Love" }
+  ];
 
-    const onSubmit = (data) => alert(JSON.stringify(data));
+  const renderPlayList = () => {
+    return playlistData.map((list, index) => (
+      <div>{`${index + 1}. ${list.title}`}</div>
+    ))
+  }
 
-    return (
-        <div id="create-playlist">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="playlist-wrapper">
-                    <div className="playlist-showcase">
-                        <div className="input-holder">
-                            <input name="playlist-name" type="text" placeholder="Playlist Name" {...register("playlistName", { required: true })} />
-                        </div>
-                        <AddShowCase showCaseData={showCaseData} />
-                    </div>
-                    <div className="playlist-CD">
-                        {!loading ? 
-                            <div className="case-box case-wrapper">
-                                <img src={CDImg} alt="" />
-                            </div>
-                        : <Skeleton width={250} height={214} className="case-box"/>}
-                        <ul>
-                            {renderPlayList()}
-                        </ul>
-                    </div>
-                </div>
-                <div className="btn-wrabtn-wrapp input-holder">
-                    <input type="submit" value="Create PlayList" />
-                </div>
-            </form>
+  const onSubmit = (data) => alert(JSON.stringify(data));
+
+  return (
+    <div id="create-playlist">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="playlist-wrapper">
+          <div className="playlist-showcase">
+            <div className="input-holder">
+              <input name="playlist-name" type="text" placeholder="Playlist Name" {...register("playlistName", { required: true })} />
+            </div>
+            <AddShowCase showCaseData={showCaseData} />
+          </div>
+          <div className="playlist-CD">
+            {loading && <Skeleton width={250} height={214} className="case-box" />}
+            <div className={`case-box case-wrapper ${loading && 'hidden'}`}>
+              <img src={CDImg} alt="" />
+            </div>
+            
+            <ul>
+              {renderPlayList()}
+            </ul>
+          </div>
         </div>
-    )
+        <div className="btn-wrabtn-wrapp input-holder">
+          <input type="submit" value="Create PlayList" />
+        </div>
+      </form>
+    </div>
+  )
 };
 
 export default CreatePlayList;
