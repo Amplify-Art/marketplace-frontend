deb6dc074a2878b1b2a96ef52be421b1667901af
anil kumar <anirai.kumar@gmail.com>
2021-06-21 14:17:50 +0530

    latest

--- src/Components/Parts/Header/index.js
+++ src/Components/Parts/Header/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import * as nearAPI from "near-api-js";
 import jwt from 'jsonwebtoken';
 import jwt_decode from 'jwt-decode';
@@ -10,90 +11,94 @@ import SearchIcon from '../../../assets/images/search-icon.svg';
 import BellIcon from '../../../assets/images/bell-icon.svg';
 import Wallet from '../../../assets/images/wallet-icon.svg';
 import Harrison from '../../../assets/images/harrison.jpeg';
+import Button from '../../Common/Button/index';
+import { displayLoadingOverlayAction } from '../../../redux/actions/GlobalAction'
 import './Header.scss';
 
 const { keyStores, WalletConnection, utils } = nearAPI;
 
-function Header( props ) {
-  // const user = jwt.decode(localStorage.getItem('amplify_app_token'));
+function Header(props) {
+  const user = jwt.decode(localStorage.getItem('amplify_app_token'));
 
-  // const [wallet, setWallet] = useState(null);
-  // const [isWalletSigned, setIsWalletSigned] = useState(user.near_connected);
-  // const [balance, setBalance] = useState(null);
-  
+  const [wallet, setWallet] = useState(null);
+  const [isWalletSigned, setIsWalletSigned] = useState(user && user.near_connected);
+  const [balance, setBalance] = useState(null);
+
   const [showWalletSidebar, toggleWalletSidebar] = useState(false);
 
   const { path } = props;
 
-  // useEffect(async () => {
-  //   const config = {
-  //     networkId: 'testnet',
-  //     keyStore: new keyStores.BrowserLocalStorageKeyStore(),                               // optional if not signing transactions
-  //     nodeUrl: 'https://rpc.testnet.near.org',
-  //     walletUrl: 'https://wallet.testnet.near.org',
-  //     helperUrl: 'https://helper.testnet.near.org',
-  //     explorerUrl: 'https://explorer.testnet.near.org'
-  //   };
-  //   const near = await nearAPI.connect(config);
-  //   const wallet = new WalletConnection(near);
-  //   setWallet(wallet)
-  // }, [])
-  // useEffect(async () => {
-  //   if (wallet && !isWalletSigned) {
-  //     wallet.requestSignIn(
-  //       // user.near_account_id,     // contract requesting access 
-  //       "Example App",                  // optional
-  //       `${window.location.origin}/near/success`,  // optional
-  //       `${window.location.origin}/near/failure`   // optional
-  //     );
-  //   }
-  // }, [wallet])
-  // const onConnect = () => {
-  //   if (wallet.isSignedIn()) {
-  //     return
-  //   } else
-  //     wallet.requestSignIn(
-  //       user.near_account_id,     // contract requesting access 
-  //       "Example App",                  // optional
-  //       `${window.location.origin}/near/success`,  // optional
-  //       `${window.location.origin}/near/failure`   // optional
-  //     );
-  // }
+  useEffect(async () => {
+    const config = {
+      networkId: 'testnet',
+      keyStore: new keyStores.BrowserLocalStorageKeyStore(),                               // optional if not signing transactions
+      nodeUrl: 'https://rpc.testnet.near.org',
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org'
+    };
+    const near = await nearAPI.connect(config);
+    const wallet = new WalletConnection(near);
+    setWallet(wallet)
+  }, [])
+  useEffect(async () => {
+    if (wallet && !isWalletSigned) {
+      wallet.requestSignIn(
+        // user.near_account_id,     // contract requesting access 
+        "Example App",                  // optional
+        `${window.location.origin}/near/success`,  // optional
+        `${window.location.origin}/near/failure`   // optional
+      );
+    }
+  }, [wallet])
 
-  // const getAccountDetails = async () => {
-  //   const config = {
-  //     networkId: 'testnet',
-  //     keyStore: new keyStores.BrowserLocalStorageKeyStore(),                               // optional if not signing transactions
-  //     nodeUrl: 'https://rpc.testnet.near.org',
-  //     walletUrl: 'https://wallet.testnet.near.org',
-  //     helperUrl: 'https://helper.testnet.near.org',
-  //     explorerUrl: 'https://explorer.testnet.near.org'
-  //   };
-  //   try {
-  //     const near = await nearAPI.connect(config);
-  //     const account = await near.account(user.near_account_id);
-      
-  //     let balances = await account.getAccountBalance();
-  //     setBalance(balances)
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
+  const onConnect = () => {
+    if (wallet.isSignedIn()) {
+      return
+    } else
+      wallet.requestSignIn(
+        "test",     // at this time, , we dont have account, passing test
+        "Example App",                  // optional
+        `${window.location.origin}/near/success`,  // optional
+        `${window.location.origin}/near/failure`   // optional
+      );
+  }
 
-  // const onCreate = async () => {
-  //   props.displayLoadingOverlay();
-  //   const create = await createWallet()
-  //   if (create.data.success) {
-  //     localStorage.setItem('amplify_app_token', create.data.token)
-  //     window.location.reload()
-  //   }
-  // }
+  const getAccountDetails = async () => {
+    const config = {
+      networkId: 'testnet',
+      keyStore: new keyStores.BrowserLocalStorageKeyStore(),                               // optional if not signing transactions
+      nodeUrl: 'https://rpc.testnet.near.org',
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org'
+    };
+    try {
+      const near = await nearAPI.connect(config);
+      const account = await near.account(user.near_account_id);
 
-  // useEffect(() => {
-  //   if (isWalletSigned) {
-  //     getAccountDetails()
-  //   }
-  // }, [isWalletSigned]);
+      let balances = await account.getAccountBalance();
+      setBalance(balances)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const onCreate = async () => {
+    props.displayLoadingOverlay();
+    return
+    const create = await createWallet()
+    if (create.data.success) {
+      localStorage.setItem('amplify_app_token', create.data.token)
+      window.location.reload()
+    }
+  }
+
+  useEffect(() => {
+    if (isWalletSigned) {
+      getAccountDetails()
+    }
+  }, [isWalletSigned]);
 
   const setBreadCrumbs = () => {
     let currentPage = '';
@@ -121,7 +126,7 @@ function Header( props ) {
   if (userToken) {
     userDetails = jwt_decode(userToken);
   }
-
+  console.log(user, 'balance')
   return (
     <>
       <header>
@@ -163,19 +168,32 @@ function Header( props ) {
       <div className="breadcrumbs left-nav-pad">
         Home / <span className="current">{path && setBreadCrumbs()}</span>
       </div>
-      
+
       {showWalletSidebar && (
         <>
           <div className="wallet-info">
             <h4>Your Wallet:</h4>
-            <p>Available Balance: $200 / 328973.21 <span className="near-icon">Ⓝ</span></p>
-            {/* {user.near_connected && balance && <div className="details">
+            {user.near_connected && balance && <div className="details">
               <h3>Account Details</h3>
+              <p>Available Balance: $200 / {balance && balance.available / 10 ** 24} <span className="near-icon">Ⓝ</span></p>
               <span>Total : <span>{Number(utils.format.formatNearAmount(balance.total)).toFixed(5)}</span></span>
               <br />
-              <span>Available : <span>{Number(utils.format.formatNearAmount(balance.available)).toFixed(5)}</span></span>
+              <span>Available : <span>{Number(utils.format.formatNearAmount(balance.available)).toFixed(5)}</span> <span className="near-icon">Ⓝ</span></span>
             </div>
-            }*/}
+            }
+            {user && !user.near_connected &&
+              < div className="buttons">
+                <Button
+                  text="Connect to Near Wallet"
+                  onClick={() => onConnect()}
+                />
+
+                <Button
+                  text="Create New Wallet"
+                  onClick={() => onCreate()}
+                />
+              </div>
+            }
           </div>
 
           <div className="sidebar-close-cover" onClick={() => toggleWalletSidebar(!showWalletSidebar)} />
@@ -185,4 +203,8 @@ function Header( props ) {
   );
 }
 
-export default withRouter(Header);
+export default connect(null, dispatch => {
+  return {
+    displayLoadingOverlay: () => dispatch(displayLoadingOverlayAction())
+  }
+})(withRouter(Header));
