a66cbb832322e78fb07063bebe734f2ad3b2c4a6
Vishal Pedhadiya <vishalpedhadiya.2611@gmail.com>
2021-06-23 10:50:33 -0700

    Added code for album-modal

---
+++ src/Components/Common/AlbumModalContent/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import playIcon from '../../../assets/images/play_icon.svg';
+import './AlbumModalContent.scss'
+
+function AlbumModalContent ({albumInfo}) {
+
+    console.log("album--->", albumInfo)
+    // title: "A Cool Album",
+    // artist: "Jonathon",
+    // totalAvailable: 100,
+    // editionNumber: 75,
+    // coverArt: CoverOne
+
+    const playListData = [
+        {icon :playIcon ,title:"What is love",Playtime:"3:43"},
+        {icon :playIcon ,title:"What is love",Playtime:"3:43"},
+        {icon :playIcon ,title:"What is love",playtime:"3:43"},
+        {icon :playIcon ,title:"What is love",playtime:"3:43"},
+        {icon :playIcon ,title:"What is love",playtime:"3:43"},
+        {icon :playIcon ,title:"What is love",playtime:"3:43"},
+    ]
+
+
+    return (
+        <div id="albums-content">
+            <div className="left-wrapper">
+                <div className="album-top">
+                    <div className="album-img">
+                        <img src={albumInfo.coverArt} alt=''/>
+                    </div>
+                    <div className="album-right">
+                        <div className="title">{albumInfo.title}</div>
+                        <div className="artist-title">{albumInfo.artist}</div>
+                        <div className="view-detail">View Details</div>
+                    </div>
+                </div>
+                <div className="album-bottom">
+                    {playListData && playListData.map((playAlbum,index) => (
+                        <div className="inner-content-album">
+                                <div><img src={playAlbum.icon} alt="" /></div>
+                                <div>{playAlbum.title}</div>
+                                <div>{playAlbum.playtime}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="bg-album-img">
+
+            </div>
+        </div>
+    )
+}
+
+export default AlbumModalContent
