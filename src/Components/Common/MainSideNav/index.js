49ad70df0015933232042be7d4d78a8b32f07f9b
Vishal Pedhadiya <vishalpedhadiya.2611@gmail.com>
2021-06-24 10:41:32 -0700

    Linked some of the routes

--- src/Components/Common/MainSideNav/index.js
+++ src/Components/Common/MainSideNav/index.js
@@ -5,20 +5,24 @@ function MainSideNav(props) {
   return (
     <div id="main-side-nav">
       <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="/">Home</a></li>
+        
         <li className="nav-header">Discover</li>
         <li><a href="#">New Releases</a></li>
         <li><a href="#">Top Charts</a></li>
-        <li><a href="#">Artists</a></li>
+        <li><a href="/artist/1">Artists</a></li>
+        
         <li className="nav-header">Store</li>
         <li><a href="#">Coming Soon</a></li>
-        <li><a href="#">Albums</a></li>
+        <li><a href="/albums">Albums</a></li>
         <li><a href="#">Songs</a></li>
+        
         <li className="nav-header">Profile</li>
-        <li><a href="#">Profile</a></li>
+        <li><a href="/my-profile">Profile</a></li>
         <li><a href="#">Nominate</a></li>
-        <li><a href="#">Wallet</a></li>
+        <li><a href="/wallet">Wallet</a></li>
         <li><a href="#">Logout</a></li>
+        
         <li className="nav-header">Artist</li>
         <li><a href="/artist-dashboard/1">Dashboard</a></li>
       </ul>
