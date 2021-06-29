4879bc1519ec8f3e2a810ed9e6db313e7a642ba6
Hardik Pedhadiya <hardikpedhadiya4199@gmail.com>
2021-06-29 19:25:20 +0530

    make social bar transparent on 404 page and after connect?create redirect to profile

--- src/Containers/PageNotFound/index.js
+++ src/Containers/PageNotFound/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import './PageNotFound.scss';
 
 function PageNotFound() {
+    useEffect(() => {
+        document.body.style.backgroundColor = '#4a4a4a'
+    },[])
     return(
         <div id="error-page" className="error-page">
             <div className="title">404</div>
