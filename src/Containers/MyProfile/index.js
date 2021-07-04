90430890be6d36ba7cc496f408efa1c7a0e7ec16
anil kumar <anirai.kumar@gmail.com>
2021-07-04 13:19:39 +0530

    fix for showing album nfts

--- src/Containers/MyProfile/index.js
+++ src/Containers/MyProfile/index.js
@@ -93,7 +93,7 @@ function MyProfile(props) {
           </div>
 
           <div className="albums" className="album-grid">
-            {props && props.nfts && props.nfts.length > 0 && props.nfts.map((nft, index) => (
+            {props && props.nfts && props.nfts.length > 0 && props.nfts.filter(f => f.type === 'album').map((nft, index) => (
               generateAlbumItem(nft, index)
             ))}
           </div>
