1fd93a5706a9026dcab868c5b65cbf0ccd4a8640
anil kumar <anirai.kumar@gmail.com>
2021-07-04 13:05:17 +0530

    fix for album modal in /my-info

--- src/Components/Common/AlbumModalContent/index.js
+++ src/Components/Common/AlbumModalContent/index.js
@@ -40,7 +40,7 @@ function AlbumModalContent({ albumInfo }) {
           </div>
           <div className="album-right">
             <div className="title">{albumInfo && albumInfo.title}</div>
-            <div className="artist-title">{albumInfo && albumInfo.user.name || 'No Artist'}</div>
+            <div className="artist-title">{albumInfo && albumInfo.currentOwner.name || 'No Artist'}</div>
             <div className="view-detail" onClick={() => setViewDetails(true)}>View Details</div>
           </div>
         </div>
