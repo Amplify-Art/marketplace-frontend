45c86ebd0a586a2d7eea0248fbef249b90a7c194
vishal pedhadiya <vishalpedhadiya.2611@gmail.com>
2021-07-06 23:47:14 +0530

    [single-album]Resoving the issue of progress bar in songs

--- src/Components/Common/AlbumSingleSong/index.js
+++ src/Components/Common/AlbumSingleSong/index.js
@@ -16,7 +16,6 @@ class ProgressRing extends React.Component {
     const { radius, stroke, progress } = this.props;
 
     const strokeDashoffset = this.circumference - progress / 100 * this.circumference;
-  
     return (
       <svg
         height={radius * 2}
@@ -26,11 +25,12 @@ class ProgressRing extends React.Component {
           stroke="white"
           fill="transparent"
           strokeWidth={ stroke }
-          strokeDasharray={ this.circumference + ' ' + this.circumference }
-          style={ { strokeDashoffset } }
+          //strokeDasharray={ this.circumference + ' ' + this.circumference }
+          // style={ { strokeDashoffset } }
           r={ this.normalizedRadius }
           cx={ radius }
           cy={ radius }
+          id="circleProgress"
          />
       </svg>
     );
@@ -45,17 +45,29 @@ function AlbumSingleSong(props) {
   const [audio] = useState(new Audio(`https://hub.textile.io/ipfs/${song.song_cid}`));
   const [playing, setPlaying] = useState(false);
   const [songProgress, setSongProgress] = useState(0);
+  const [audioTime, setAudioTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const toggle = () => setPlaying(!playing);
   
   useEffect(() => {
     playing ? audio.play() : audio.pause();
-  }, [audio, playing]);
+  }, [audio, playing,audio.currentTime,audio.duration]); 
 
   useEffect(() => {
-    audio.addEventListener("ended", () => setPlaying(false));
+    audio.addEventListener("ended", () =>{ 
+      setPlaying(false)
+    });
+    audio.addEventListener("timeupdate", e => {
+      const progressElement = document.getElementById('circleProgress')
+        progressElement.style.strokeDasharray = `${audio.currentTime},${audio.duration}` 
+        // setAudioTime(audio.currentTime)
+        // setDuration(audio.duration)
+        // setSongProgress(e.currentTime / e.duration)
+    });
     return () => {
       audio.removeEventListener("ended", () => setPlaying(false));
+      audio.removeEventListener("timeupdate", () => {setSongProgress(0)});
     };
   },[playing]);
 
@@ -71,6 +83,8 @@ function AlbumSingleSong(props) {
                 radius={ 13 }
                 stroke={ 2 }
                 progress={ audio.currentTime / audio.duration }
+                // time = {audioTime}
+                // duration = {duration}
               />
             </div>
           ) : (
