aaa9cebb73d0e7d8ca6cc789e4c1100ece8ba9b5
vishal pedhadiya <vishalpedhadiya.2611@gmail.com>
2021-07-08 22:19:59 +0530

    [single-ablum]when a user closes the modal, it will stop playing the song

--- src/Components/Common/AlbumSingleSong/index.js
+++ src/Components/Common/AlbumSingleSong/index.js
@@ -5,13 +5,13 @@ import playIcon from '../../../assets/images/play_icon.svg';
 class ProgressRing extends React.Component {
   constructor(props) {
     super(props);
-    
+
     const { radius, stroke } = this.props;
-    
+
     this.normalizedRadius = radius - stroke * 2;
     this.circumference = this.normalizedRadius * 2 * Math.PI;
   }
-  
+
   render() {
     const { radius, stroke, progress } = this.props;
 
@@ -19,17 +19,17 @@ class ProgressRing extends React.Component {
       <svg
         height={radius * 2}
         width={radius * 2}
-       >
+      >
         <circle
           stroke="white"
           fill="transparent"
-          strokeWidth={ stroke }
-          strokeDasharray={ 0 + ' ' + 0 }
-          r={ this.normalizedRadius }
-          cx={ radius }
-          cy={ radius }
+          strokeWidth={stroke}
+          strokeDasharray={0 + ' ' + 0}
+          r={this.normalizedRadius}
+          cx={radius}
+          cy={radius}
           id="circleProgress"
-         />
+        />
       </svg>
     );
   }
@@ -38,38 +38,40 @@ class ProgressRing extends React.Component {
 const audioElement = new Audio();
 
 function AlbumSingleSong(props) {
-  const { song, index } = props;
-  
+  const { song, index, isOpen } = props;
+
   const [audio] = useState(new Audio(`https://hub.textile.io/ipfs/${song.song_cid}`));
   const [playing, setPlaying] = useState(false);
   const [songProgress, setSongProgress] = useState(0);
-
   const toggle = () => setPlaying(!playing);
-  
+
+  useEffect(() => {
+    !isOpen && setPlaying(false)
+  }, [isOpen])
+
   useEffect(() => {
     playing ? audio.play() : audio.pause();
-  }, [audio, playing,audio.currentTime,audio.duration]); 
+  }, [audio, playing, audio.currentTime, audio.duration]);
 
   useEffect(() => {
-    audio.addEventListener("ended", () =>{ 
+    audio.addEventListener("ended", () => {
       setPlaying(false)
     });
     audio.addEventListener("timeupdate", e => {
       const progressElement = document.getElementById('circleProgress')
-      if(progressElement)
-      {
+      if (progressElement) {
         let normalizedRadius = 9;
         let circumference = normalizedRadius * 2 * Math.PI;
-        let startPoint = (audio.currentTime / audio.duration)* circumference;
+        let startPoint = (audio.currentTime / audio.duration) * circumference;
         let endPoint = circumference - (audio.currentTime / audio.duration) / 100 * circumference;
         progressElement.style.strokeDasharray = `${startPoint},${endPoint}`
       }
     });
     return () => {
       audio.removeEventListener("ended", () => setPlaying(false));
-      audio.removeEventListener("timeupdate", () => {setSongProgress(0)});
+      audio.removeEventListener("timeupdate", () => { setSongProgress(0) });
     };
-  },[playing]);
+  }, [playing]);
 
   return (
     <div className="inner-content-album" key={`al${index}`}>
@@ -78,9 +80,9 @@ function AlbumSingleSong(props) {
           {playing ? (
             <div onClick={toggle}>
               <ProgressRing
-                radius={ 13 }
-                stroke={ 2 }
-                progress={ audio.currentTime / audio.duration }
+                radius={13}
+                stroke={2}
+                progress={audio.currentTime / audio.duration}
               />
             </div>
           ) : (
