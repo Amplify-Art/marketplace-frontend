baaebd3728a81cc092fdf1b35c405aa4e1ebc282
Jonathon <jonathon@pixel8.io>
2021-08-16 11:11:49 -0400

    fix for overlay loader covering sidebar

--- src/Components/Common/Loading/index.jsx
+++ src/Components/Common/Loading/index.jsx
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 import './loader.scss';
 
 const LO = ({ children, active, text }) => {
-    return (
-        <LoadingOverlay
-            active={active}
-            spinner
-            className={`loader ${active ? 'active' : ''}`}
-            text={text}
-        >
-            {children}
-        </LoadingOverlay>
-    )
+  return (
+    <LoadingOverlay
+      active={active}
+      spinner
+      className={`loader ${active ? 'active' : ''}`}
+      text={text}
+    >
+      {children}
+    </LoadingOverlay>
+  )
 }
 export default connect(state => {
-    return {
-        active: state.global.loading_overlay
-    }
+  return {
+    active: state.global.loading_overlay
+  }
 })(LO);
