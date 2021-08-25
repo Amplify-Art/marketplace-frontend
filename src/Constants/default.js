8505781a1eab7e4fc85ef0055065543f876b3791
anil kumar <anirai.kumar@gmail.com>
2021-08-25 16:06:27 +0530

    staging url

--- src/Constants/default.js
+++ src/Constants/default.js
@@ -2,6 +2,8 @@
 let API_ENDPOINT_URL;
 if (window.location.origin == "https://amplifyart.netlify.app") {
     API_ENDPOINT_URL = "https://amplify-api.herokuapp.com";
+} else if (window.location.origin == "https://staging-amplify.netlify.app/") {
+    API_ENDPOINT_URL = "https://staging-amplify-api.herokuapp.com";
 } else {
     API_ENDPOINT_URL = 'http://localhost:8000'
     // API_ENDPOINT_URL = 'https://amplify-api.herokuapp.com';
