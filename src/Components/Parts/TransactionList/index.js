ab02c268c0201b105cb31de43c61575a5a8f8a87
BhatSudesh <sudesh@pixel8.io>
2021-09-20 11:22:23 +0530

    Done with wallet responsive

--- src/Components/Parts/TransactionList/index.js
+++ src/Components/Parts/TransactionList/index.js
@@ -3,13 +3,14 @@ import moment from 'moment';
 import './TransactionList.scss';
 
 function TransactionList(props) {
-
+  const screenWidth = window.screen.availWidth;
   const textEllipsis = (txt) => {
     if (txt.length > 13) {
       return txt.substr(0, 9) + '...' + txt.substr(txt.length - 4, txt.length);
     }
     return txt;
-  }
+  };
+
   return (
     <div className="transactionWrapper">
       <table className="transTable">
@@ -18,8 +19,22 @@ function TransactionList(props) {
             ?
             props.transactionList.map(transaction => (
               <tr className="transRow" onClick={() => props.onClickItem(transaction)}>
-                <td className="transId">{textEllipsis(transaction.transaction_hash || '')}</td>
-                <td className="transDate">{moment(transaction.created_at).format('MMM DD, YYYY')}</td>
+                {
+                  screenWidth < 600
+                    ? <td className="transId">
+                        <div className="transIdHolder">
+                          {textEllipsis(transaction.transaction_hash || '')}
+                        </div>
+                        <div className="transDateHolder">
+                          {moment(transaction.created_at).format('MMM DD, YYYY')}
+                        </div>
+                      </td>
+                    : <td className="transId">{textEllipsis(transaction.transaction_hash || '')}</td>
+                }
+                {
+                  screenWidth > 600 &&
+                  <td className="transDate">{moment(transaction.created_at).format('MMM DD, YYYY')}</td>
+                }
                 <td className="transAmt">
                   <div className={String(transaction.price).includes('-') ? 'redTxt' : 'greenTxt'}>{String(transaction.price).includes('-') ? (transaction.price_in_yocto_near / 10 ** 24).toFixed(4) + ' NEAR' : '+' + (transaction.price_in_yocto_near / 10 ** 24).toFixed(4) + ' NEAR'}</div>
                   <div className="smallTxt">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(Math.abs(transaction.price) / 100)}</div>
