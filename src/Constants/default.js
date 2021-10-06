ac7737ad906b3c732f90aa57d772a524fc797808
Anil Kumar M C <anilkumarmc@MacBook-Air.local>
2021-10-06 11:53:00 +0530

    ebs api url

--- src/Constants/default.js
+++ src/Constants/default.js
@@ -4,6 +4,8 @@ if (window.location.origin == "https://amplifyart.netlify.app") {
     API_ENDPOINT_URL = "https://amplify-api.herokuapp.com";
 } else if (window.location.origin == "https://staging-amplify.netlify.app") {
     API_ENDPOINT_URL = "https://staging-amplify-api.herokuapp.com";
+} else if (window.location.origin == "https://prod-amplifyart.netlify.app/") {
+    API_ENDPOINT_URL = "http://amplifyapi-production.eba-bchixipm.us-east-1.elasticbeanstalk.com";
 } else {
     API_ENDPOINT_URL = 'http://localhost:8000'
     // API_ENDPOINT_URL = 'https://amplify-api.herokuapp.com';
