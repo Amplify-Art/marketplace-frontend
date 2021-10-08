90eaa1bdffa1edc91390e8a48bd496bd6f1bd135
Anil Kumar M C <anilkumarmc@MacBook-Air.local>
2021-10-08 18:30:14 +0530

    new urls

--- src/Constants/default.js
+++ src/Constants/default.js
@@ -6,6 +6,8 @@ if (window.location.origin == "https://amplifyart.netlify.app") {
     API_ENDPOINT_URL = "https://staging-amplify-api.herokuapp.com";
 } else if (window.location.origin == "https://prod-amplifyart.netlify.app") {
     API_ENDPOINT_URL = "http://amplifyapi-production.eba-bchixipm.us-east-1.elasticbeanstalk.com";
+} else if (window.location.origin == "https://amplify.art") {
+    API_ENDPOINT_URL = "http://api.amplify.art";
 } else {
     API_ENDPOINT_URL = 'http://localhost:8000'
     // API_ENDPOINT_URL = 'https://amplify-api.herokuapp.com';
