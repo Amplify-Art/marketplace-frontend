53273e0a81f5d2e46e9bd94e6fc9656ae6e542e4
Anil Kumar M C <anilkumarmc@MacBook-Air.local>
2021-10-25 20:39:55 +0530

    fix for auto connect

--- src/Components/Parts/Header/index.js
+++ src/Components/Parts/Header/index.js
@@ -85,7 +85,7 @@ function Header(props) {
     if (wallet && !isWalletSigned) {
       console.log('SHOULD REQUEST for Signing in')
       wallet.requestSignIn(
-        process.env.REACT_APP_CONTEXT === 'production' ? "amplifyapp.near" : "pixeltest2.testnet",     // contract requesting access 
+        // process.env.REACT_APP_CONTEXT === 'production' ? "amplifyapp.near" : "pixeltest2.testnet",     // contract requesting access 
         "Example App",                  // optional
         `${window.location.origin}/near/success`,  // optional
         `${window.location.origin}/near/failure`   // optional
