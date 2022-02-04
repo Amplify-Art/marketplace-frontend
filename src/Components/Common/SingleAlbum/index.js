a7ec1a9275df78d62ff3266890ab3b2d51ecd643
Anil Kumar M C <anirai.kumar@gmail.com>
2022-02-04 15:22:46 +0530

    upgraded contract

--- src/Components/Common/SingleAlbum/index.js
+++ src/Components/Common/SingleAlbum/index.js
@@ -147,10 +147,10 @@ function SingleAlbum(props) {
 
       localStorage.setItem('album_bundle_info', JSON.stringify(album_bundle_info))
       await (props.wallet.account()).functionCall(
-        process.env.REACT_APP_NEAR_MARKET_ACCOUNT || 'market.aatest.testnet',
+        process.env.REACT_APP_NEAR_MARKET_ACCOUNT || 'market.aa-1-test.testnet',
         'offer_album',
         {
-          nft_contract_id: process.env.REACT_APP_NFT_CONTRACT || 'nft.aatest.testnet',
+          nft_contract_id: process.env.REACT_APP_NFT_CONTRACT || 'nft.aa-1-test.testnet',
           albumipfs_hash_copy: `${albumInfo.cover_cid}:${copy_no}`,
         },
         200000000000000,
--- src/Containers/MyProfile/index.js
+++ src/Containers/MyProfile/index.js
@@ -203,13 +203,13 @@ function MyProfile(props) {
 
       localStorage.setItem("selling_song", JSON.stringify(selling_song));
       await props.wallet.account().functionCall(
-        process.env.REACT_APP_NFT_CONTRACT || "nft.aatest.testnet",
+        process.env.REACT_APP_NFT_CONTRACT || "nft.aa-1-test.testnet",
         "nft_approve",
         {
           token_id: songtokenid,
           account_id:
             process.env.REACT_APP_NEAR_MARKET_ACCOUNT ||
-            "market.aatest.testnet",
+            "market.aa-1-test.testnet",
           price: parseNearAmount(`${nearPrice}`),
         },
         300000000000000,
