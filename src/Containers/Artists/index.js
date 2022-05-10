96e81391d0db302eecf22fdbfe408f1f51089dc6
Anil Kumar M C <anirai.kumar@gmail.com>
2022-05-10 12:42:03 +0530

    default artist images

--- src/Containers/Artists/index.js
+++ src/Containers/Artists/index.js
@@ -1,42 +1,53 @@
-import React, { useEffect } from 'react';
-import { withRouter, useHistory } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchArtists } from '../../redux/actions/ArtistAction';
-import './Artists.scss';
-
-import UserAvatar from '../../Components/Common/UserAvatar/index';
+import React, { useEffect } from "react";
+import { withRouter, useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchArtists } from "../../redux/actions/ArtistAction";
+import "./Artists.scss";
+import defaultProfile from "../../assets/images/default-profile.jpg";
+import UserAvatar from "../../Components/Common/UserAvatar/index";
 
 function Artists(props) {
-  const {fetchArtists, artists} = props;
+  const { fetchArtists, artists } = props;
   useEffect(() => {
     fetchArtists({
       params: {
-        'filter[type]': 'artist',
-      }
+        "filter[type]": "artist",
+      },
     });
   }, []);
+  console.log(artists, "artists");
   return (
     <div id="artists-page" className="left-nav-pad right-player-pad">
       <div className="container">
         <h2 className="page-title">Artists</h2>
-          <div className="artists-holder">
-            {artists && artists.length && artists.map((artist, index) => (
-              <UserAvatar avatarImg={artist.avatar} key={index} name={artist.name} onClick={() => props.history.push(`/artist/${artist.near_account_id}`)} />
+        <div className="artists-holder">
+          {artists &&
+            artists.length &&
+            artists.map((artist, index) => (
+              <UserAvatar
+                avatarImg={artist.avatar || defaultProfile}
+                key={index}
+                name={artist.near_account_id}
+                onClick={() =>
+                  props.history.push(`/artist/${artist.near_account_id}`)
+                }
+              />
             ))}
-          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default connect(state => {
-  return {
-    artists: state.artist.artists
-  }
-},
-  dispatch => {
+export default connect(
+  (state) => {
     return {
-      fetchArtists: (payload) => dispatch(fetchArtists(payload))
-    }
-  })(withRouter(Artists));
-
+      artists: state.artist.artists,
+    };
+  },
+  (dispatch) => {
+    return {
+      fetchArtists: (payload) => dispatch(fetchArtists(payload)),
+    };
+  }
+)(withRouter(Artists));
