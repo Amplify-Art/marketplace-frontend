c10b81f39d4ce0a866ccbd21de2f5e638b78f7fb
Russ <russ@attackwagon.com>
2022-12-09 12:55:28 -0500

    commit before branch switch

--- src/Components/Parts/Banner/index.js
+++ src/Components/Parts/Banner/index.js
@@ -8,13 +8,13 @@ function Banner(props) {
       <div id="banner">
           <div className="lg-text">
             <div className="top">
-              <h1>{homeContent.banner_text_line_one}</h1>
+              <h1>Test Banner Content</h1>
               <div className="sub-text">
                 <div className="line" />
-                <p>{homeContent.banner_sub_text}</p>
+                <p>sub content test</p>
               </div>
             </div>
-            <h1 className="red">{homeContent.banner_text_line_two}</h1>
+            <h1 className="red">some cool red text</h1>
           </div>
 
           <div className="triangle-image"><img src={Triangle} /></div>
