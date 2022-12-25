1cbcd1b7b1ef9523052197429befaa5e52eb9d94
Russ <russ@thingscorp.com>
2022-12-25 11:49:46 -0500

    Christmas Push

--- src/Components/Parts/ThankYou/index.js
+++ src/Components/Parts/ThankYou/index.js
@@ -5,7 +5,7 @@ function ThankYou(props) {
   return (
     <div id="thank-you">
       <div className="container center-text">
-        <h2 className="large center-text">Thank <span className="red">You</span></h2>
+        <h2 className="large center-text">JOIN <span className="red">NOW</span></h2>
         <p>{content}</p>
         <a href="#" className="btn btn-red">Join Today</a>
       </div>
