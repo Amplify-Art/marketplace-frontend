89d299fa490856bcba8b6bf878cb0475a19dc666
liyichong94615 <liyichong94@gmail.com>
2023-01-15 07:48:01 +0800

    Shimmer-Albums and artists

---
+++ src/Components/Common/SingleAlbum/shimmer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SingleAlbum.scss";
+
+function SingleAlbumShimmer() {
+  return (
+    <div
+      className="single-album1"
+    >
+      <div className="shimmer-custom rect"></div>
+      <div className="shimmer-custom title"></div>
+      <div className="shimmer-custom text"></div>
+      <div className="shimmer-custom sub-text"></div>
+    </div>
+  );
+}
+
+export default SingleAlbumShimmer;
