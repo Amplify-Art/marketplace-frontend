76eb5b65a39aa036a75374e069bc6446ff6db0bd
Russ <russ@thingscorp.com>
2023-01-26 22:23:23 -0500

    Revert "Reverted to old contracts"
    This reverts commit 75a8abe7777c8da66ba367406b6436d06cb45f4a.

--- src/Components/Common/SingleAlbum/index.js
+++ src/Components/Common/SingleAlbum/index.js
@@ -164,11 +164,11 @@ function SingleAlbum(props) {
       JSON.stringify(album_bundle_info)
     );
     await props.wallet.account().functionCall(
-      process.env.REACT_APP_NEAR_MARKET_ACCOUNT || "market1.amplifybeta.testnet",
+      process.env.REACT_APP_NEAR_MARKET_ACCOUNT || "marketnew.amplifytmp.testnet",
       "offer_album",
       {
         nft_contract_id:
-          process.env.REACT_APP_NFT_CONTRACT || "nft1.amplifybeta.testnet",
+          process.env.REACT_APP_NFT_CONTRACT || "nftnew.amplifytmp.testnet",
         albumipfs_hash_copy: `${albumInfo.cover_cid}:${copy_no}`,
       },
       200000000000000,
