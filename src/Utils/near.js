59520eeda212caff061d8b3ff5d1c9dd1d6eec65
liyichong94615 <liyichong94@gmail.com>
2023-02-15 02:28:24 +0800

    Updated v21

--- src/Utils/near.js
+++ src/Utils/near.js
@@ -2,7 +2,7 @@ import * as nearAPI from "near-api-js";
 export const getTokens = async (wallet) => {
   const contract = new nearAPI.Contract(
     wallet.account(), // the account object that is connecting
-    process.env.REACT_APP_NFT_CONTRACT || "nft_v20.amplifyart.testnet",
+    process.env.REACT_APP_NFT_CONTRACT || "nft_v21.amplifyart.testnet",
     {
       // name of contract you're connecting to
       viewMethods: ["nft_tokens_for_owner"], // view methods do not change state but usually return a value
